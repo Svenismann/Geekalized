38542c020df2e2165beca24fb346be9d48145ffc
Sven Forstner <sforstner700@gmail.com>
2017-12-24 22:32:29 +0100

    Initial Commit

---
+++ Geekalized/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geekalized
+//
+//  Created by Sven Forstner on 24.12.17.
+//  Copyright © 2017 Sven Forstner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
